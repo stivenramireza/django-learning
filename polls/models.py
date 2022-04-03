cf54d50236019cb1735abff8d58918a26a335653
Stiven Ramírez Arango <stivenramireza@gmail.com>
2022-04-03 18:22:15 -0500

    Models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    question_text = models.CharField("Question text", max_length=200)
+    pub_date = models.DateTimeField("Published date")
+
+    def __str__(self) -> str:
+        return self.question_text
+
+    def was_published_recently(self) -> bool:
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField("Choice text", max_length=200)
+    votes = models.IntegerField("Votes", default=0)
+
+    def __str__(self) -> str:
+        return self.choice_text
--- polls/views.py
+++ polls/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request: object) -> None:
-    return HttpResponse("Hello world!")
+def index(request: object) -> object:
+    return HttpResponse("You are in the main page")
+
+
+def detail(request: object, question_id: int) -> object:
+    return HttpResponse(f"You are watching the question #{question_id}")
+
+
+def results(request: object, question_id: int) -> object:
+    return HttpResponse(f"You are watching the results of the question #{question_id}")
+
+
+def results(request: object, question_id: int) -> object:
+    return HttpResponse(f"You are voting the question #{question_id}")
