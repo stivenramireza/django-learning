bad2e8b520347d73227b87dd151cc79f3789fc84
Stiven Ramírez Arango <stivenramireza@gmail.com>
2022-04-04 15:40:18 -0500

    Adding more tests

--- polls/tests.py
+++ polls/tests.py
@@ -1,37 +1,113 @@
+from cgitb import text
 import datetime
+from venv import create
 
 from django.test import TestCase
 from django.utils import timezone
+from django.urls.base import reverse
 
 from .models import Question
 
 
-class TestQuestionModel(TestCase):
-    def setUp(self) -> None:
-        self.question = Question(
-            question_text="¿Quién es el mejor Course Director de Platzi?"
-        )
+def create_question(text: str, days: int) -> Question:
+    """
+    Creates a question with the given text and days. Remember that positive
+    days is applied to future questions and negative days to past questions.
 
-    def test_was_published_recently_with_future_questions(self):
+    Args:
+        text: Description of the question.
+        days: Number of days in the future or past.
+
+    Returns:
+        The created question in the database.
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    question = Question.objects.create(question_text=text, pub_date=time)
+    return question
+
+
+class TestQuestionModel(TestCase):
+    def test_was_published_recently_with_future_questions(self) -> None:
         """
         was_published_recently() returns False for questions whose pub_date
         is in the future.
         """
-        self.question.pub_date = timezone.now() + datetime.timedelta(days=30)
-        self.assertIs(self.question.was_published_recently(), False)
+        future_question = create_question(text="Future question", days=30)
+        self.assertIs(future_question.was_published_recently(), False)
 
-    def test_was_published_recently_with_present_questions(self):
+    def test_was_published_recently_with_present_questions(self) -> None:
         """
         was_published_recently() returns True for questions whose pub_date is
         in the present.
         """
-        self.question.pub_date = timezone.now()
-        self.assertIs(self.question.was_published_recently(), True)
+        present_question = create_question(text="Present question", days=0)
+        self.assertIs(present_question.was_published_recently(), True)
 
-    def test_was_published_recently_with_past_questions(self):
+    def test_was_published_recently_with_past_questions(self) -> None:
         """
         was_published_recently() returns False for questions whose pub_date is
         in the present.
         """
-        self.question.pub_date = timezone.now() - datetime.timedelta(days=30)
-        self.assertIs(self.question.was_published_recently(), False)
+        past_question = create_question(text="Past question", days=-15)
+        self.assertIs(past_question.was_published_recently(), False)
+
+
+class TestQuestionIndexView(TestCase):
+    def test_no_questions(self) -> None:
+        """
+        If no question exists, an appropiate message is displayed.
+        """
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context["latest_question_list"], [])
+
+    def test_future_questions(self) -> None:
+        """
+        If future questions are present, they won't display in the page.
+        """
+        create_question(text="Future question", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context["latest_question_list"], [])
+
+    def test_past_questions(self) -> None:
+        """
+        If future questions are present, they won't display in the page.
+        """
+        past_question = create_question(text="Past question", days=-7)
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context["latest_question_list"], [past_question]
+        )
+
+    def test_future_and_past_questions(self) -> None:
+        """
+        Even if both past and future questions exist, only past questions are displayed.
+        """
+        past_question = create_question(text="Past question", days=-4)
+        create_question(text="Future question", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerysetEqual(
+            response.context["latest_question_list"], [past_question]
+        )
+
+    def test_two_past_questions(self) -> None:
+        """
+        The questions index page may display multiple questions.
+        """
+        past_question_1 = create_question(text="Past question 1", days=-4)
+        past_question_2 = create_question(text="Past question 2", days=-8)
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context["latest_question_list"], [past_question_1, past_question_2]
+        )
+
+    def test_two_future_questions(self) -> None:
+        create_question(text="Future question 1", days=9)
+        create_question(text="Future question 2", days=21)
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context["latest_question_list"], [])
--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -18,7 +19,9 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self) -> object:
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
 
 
 # def detail(request: object, question_id: int) -> object:
