2dc8c13dee4e867ba272c4130529aafe564b3d61
Stiven Ramírez Arango <stivenramireza@gmail.com>
2022-04-04 14:06:19 -0500

    More test cases

--- polls/models.py
+++ polls/models.py
@@ -12,7 +12,11 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self) -> bool:
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return (
+            timezone.now()
+            >= self.pub_date
+            >= timezone.now() - datetime.timedelta(days=1)
+        )
 
 
 class Choice(models.Model):
--- polls/tests.py
+++ polls/tests.py
@@ -7,13 +7,31 @@ from .models import Question
 
 
 class TestQuestionModel(TestCase):
+    def setUp(self) -> None:
+        self.question = Question(
+            question_text="¿Quién es el mejor Course Director de Platzi?"
+        )
+
     def test_was_published_recently_with_future_questions(self):
         """
-        was_published_recently returns False for questions whose pub_date
+        was_published_recently() returns False for questions whose pub_date
         is in the future.
         """
-        time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(
-            question_text="¿Quién es el mejor Course Director de Platzi?", pub_date=time
-        )
-        self.assertIs(future_question.was_published_recently(), False)
+        self.question.pub_date = timezone.now() + datetime.timedelta(days=30)
+        self.assertIs(self.question.was_published_recently(), False)
+
+    def test_was_published_recently_with_present_questions(self):
+        """
+        was_published_recently() returns True for questions whose pub_date is
+        in the present.
+        """
+        self.question.pub_date = timezone.now()
+        self.assertIs(self.question.was_published_recently(), True)
+
+    def test_was_published_recently_with_past_questions(self):
+        """
+        was_published_recently() returns True for questions whose pub_date is
+        in the present.
+        """
+        self.question.pub_date = timezone.now() - datetime.timedelta(days=30)
+        self.assertIs(self.question.was_published_recently(), False)
